8d274925acad953ea127c5fdc035830f9c75b7d3
wtl <313412736@qq.com>
2016-08-15 17:31:07 +0800

    first commit

--- Exam_four/src/main/java/javabasexam/App.java
+++ Exam_four/src/main/java/javabasexam/App.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 /*
  * 随机生成 50 个小于 100 的整数,放入 List 中,将 List 中的数据除以 10,
- * 以结果的整数值作为 key 放入 Map 中,得到如{1=>[11,10,12],2=>[21,24,23]}的 Map,
+ * 得到如{1=>[11,10,12],2=>[21,24,23]}的 Map,
  * 再将 Map 中 key 对应的数组进行排序,得到如{1=>[10,11,12],2=>[21,23,24]}
  * 输出的形式如下:
  * 		随机生成 50 个小于 100 的数 , 分别为 : xxx,xxxx,xxxx
@@ -17,7 +17,7 @@ public class App {
 	public static void main(String[] args) {
 
 		List<Integer> list = new ArrayList<Integer>();
-		List<Integer> li = new ArrayList<Integer>();
+	
 		Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>(); 
 		for(int i=0;i<50;i++){
 			list.add((int)(Math.random()*100));
@@ -27,9 +27,12 @@ public class App {
 		for (int i = 0; i < list.size(); i++) {
 			System.out.print(list.get(i)+",");
 			int list1 = list.get(i);
-			int a = list1%10;
-			//			li = map.get(a);
-			//			li.add(list1);
+			int a = list1/10;
+			List<Integer> li = new ArrayList<Integer>();
+			if(map.containsKey(a)){
+				li = map.get(a);
+			}
+			li.add(list1);
 			map.put(a,li);
 		}
 		System.out.println();
